718a38397ce83412a2e88c88c40f819a26f8967f
LimtlessKieran <sylvkn@farmingdale.edu>
2022-11-09 18:12:10 -0500

    need skybox again

---
+++ Assets/Scripts/DestroyTheme.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTheme : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(GameObject.Find("MusicManager"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
