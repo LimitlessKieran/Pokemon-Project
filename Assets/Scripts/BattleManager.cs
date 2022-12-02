b9de13a0d57d77e8ef7af0654d4c0324c3dcf07b
LimtlessKieran <sylvkn@farmingdale.edu>
2022-12-01 21:21:32 -0500

    made opponent team

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -15,12 +15,36 @@ public class BattleManager : MonoBehaviour
     Vector3 away = new Vector3(25.4567f, 0.15f, 33.15f);
 
     public List<string> battleTeam = new List<string>();
+    public List<string> opponentTeam = new List<string>();
+    public List<string> allPokemon = new List<string>();
+
         
     // Start is called before the first frame update
     void Start()
     {
+        int count = 0;
         go = GameObject.Find("TeamCreater");
+
+        allPokemon.Add("Charizard");
+        allPokemon.Add("Sceptile");
+        allPokemon.Add("Pikachu");
+        allPokemon.Add("Feraligatr");
+        allPokemon.Add("Aerodactyl");
+        allPokemon.Add("Gallade");
+
         battleTeam = go.GetComponent<SelectPokemon>().PokemonTeam;
+        for (int i = 0; i < allPokemon.Count - 1; i++)
+        {
+
+            if (battleTeam.Contains(allPokemon[i]))
+            {
+            
+            }
+            else
+            {
+                opponentTeam.Add(allPokemon[i]);
+            }
+        }
     }
 
     // Update is called once per frame
