f20c61fe27c15ed1da5570c7491ebbf799e04d81
LimtlessKieran <sylvkn@farmingdale.edu>
2022-12-01 21:45:45 -0500

    Pokemon are facing each other

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -6,18 +6,20 @@ using TMPro;
 public class BattleManager : MonoBehaviour
 {
     public GameObject go;
-    public GameObject Pokemon1;
-    public GameObject Pokemon2;
-    public GameObject Pokemon3;
+    public GameObject myPokemon , opponentPokemon;
+   
 
     Vector3 yourTeam = new Vector3(42.59f, .009f, 44.98f); //6868628
     Vector3 oppTeam = new Vector3(25.4567f, 0.15f, 33.15f);
     Vector3 away = new Vector3(25.4567f, 0.15f, 33.15f);
+    Vector3 pivot = new Vector3(34f, 0f, 38f);
+
 
     public static List<string> battleTeam = new List<string>();
     public static List<string> opponentTeam = new List<string>();
     public List<string> allPokemon = new List<string>();
 
+   
 
     // Start is called before the first frame update
     void Start()
@@ -45,6 +47,15 @@ public class BattleManager : MonoBehaviour
                 opponentTeam.Add(allPokemon[i]);
             }
         }
+
+        myPokemon = GameObject.Find(battleTeam[0]);
+        myPokemon.transform.position = yourTeam;
+        myPokemon.transform.LookAt(pivot);
+
+        opponentPokemon = GameObject.Find(opponentTeam[0]);
+        opponentPokemon.transform.position = oppTeam;
+        opponentPokemon.transform.LookAt(pivot);
+
     }
 
     // Update is called once per frame
